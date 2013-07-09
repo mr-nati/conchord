04069e5091b4997b60ab74097900a8f0add2e25a
Nati <nati.tessema@gmail.com>
2013-07-09 02:33:17 -0700

    Got a tiny media player working.

--- android/conchord/src/com/conchord/android/MainActivity.java
+++ android/conchord/src/com/conchord/android/MainActivity.java
@@ -1,15 +1,64 @@
 package com.conchord.android;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	Button buttonPlay;
+	Button buttonFfwd;
+	MediaPlayer mPlayer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// set up MediaPlayer
+		mPlayer = MediaPlayer.create(getApplicationContext(),
+				R.raw.the_alan_parsons_project_sirius);
+		mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+		buttonPlay = (Button) findViewById(R.id.button_play);
+		buttonFfwd = (Button) findViewById(R.id.button_ffwd);
+
+		buttonPlay.setText("play");
+		buttonPlay.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if (mPlayer.isPlaying()) {
+					Toast.makeText(getApplicationContext(), "already playing",
+							Toast.LENGTH_SHORT).show();
+				} else {
+					mPlayer.start();
+					buttonPlay.setText("pause");
+				}
+			}
+		});
+
+		buttonFfwd.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if (mPlayer.isPlaying()) {
+					mPlayer.pause();
+					mPlayer.seekTo(mPlayer.getCurrentPosition() + 5000);
+					mPlayer.start();
+				} else {
+					Toast.makeText(getApplicationContext(), "press play first",
+							Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+
 	}
 
 	@Override
